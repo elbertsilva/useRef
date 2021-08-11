15d9efd3c9ef25ab040bff88fe728eae28ed1371
elbertmartins <elbert.martins@conforce.com.br>
2021-08-11 13:53:19 -0300

    seRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const [carrinho, setCarrinho] = React.useState(0);
+  const [notification, setNotification] = React.useState(null);
+
+  function handleClick() {
+    setCarrinho(carrinho + 1);
+    setNotification("Item adicionado ao carrinho");
+    setTimeout(() => {
+      setNotification(null);
+    }, 2000);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{notification}</p>
+      <button onClick={handleClick}>Adicionar Carrinho {carrinho}</button>
     </div>
   );
-}
+};
 
 export default App;
